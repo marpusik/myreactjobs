1d2fde27d7518a439ed28850e13cb47cac6defb7
marpusik <marpusik1987@gmail.com>
2021-05-13 16:36:57 +0300

    footer 2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 // import logo from './logo.svg';
 import './App.css';
 import Header from './pages/header/Header';
-
-
 import Main from './pages/main/Main';
+import Footer from "./pages/footer/Footer";
 
 function App() {
 
 
-  return (
-    <div className="App">
-      <div className="container">
-
-        <Header />
-        <Main />
-
-
-      </div>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="container">
+                <Header/>
+                <Main/>
+                <Footer/>
+            </div>
+        </div>
+    );
 }
 
 export default App;
