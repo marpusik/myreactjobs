e92fe740a2ec4820cf03d1b7769fe45bccdc1183
marpusik <marpusik1987@gmail.com>
2021-05-17 20:18:15 +0300

    footer 3

--- src/pages/footer/contract/Сontract.js
+++ src/pages/footer/contract/Сontract.js
@@ -3,18 +3,15 @@ import s from './Сontract.module.scss'
 function Contract() {
     return (
         <div className="contract">
-        <ul className={s.contract__list}>
+            <ul className={s.contract__list}>
                 <li className="contract__item"><a className="contract__link"
-                                              href="ya.ru">Договор оферты</a></li>
+                                                  href="">Договор оферты</a></li>
                 <li className="contract__item"><a className="contract__link"
-<<<<<<< HEAD
-                                              href="">Политика обработки персональных данных</a></li>
+                                                  href="">Политика обработки персональных данных</a></li>
+                <li className="contract__item"><a className="contract__link"
+                                                  href="">Политика обработки персональных данных</a></li>
             </ul>
-            </div>
-=======
-                                              href="ya.ru">Политика обработки персональных данных</a></li>
-            </ul></>
->>>>>>> main
+        </div>
     );
 }
 
--- src/pages/footer/menu/Menu.js
+++ src/pages/footer/menu/Menu.js
@@ -3,23 +3,18 @@ import s from './Menu.module.scss';
 function Menu() {
     return (
         <div className={s.menu}>
-        <h3 className="menu__title">Информация</h3>
+            <h3 className="menu__title">Информация</h3>
             <ul className="menu__list">
                 <li className="menu__item"><a className="menu__link"
-                                              href="ya.ru">О компании</a></li>
+                                              href="">О компании</a></li>
                 <li className="menu__item"><a className="menu__link"
-                                              href="ya.ru">Контакты</a></li>
+                                              href="">Контакты</a></li>
                 <li className="menu__item"><a className="menu__link"
-                                              href="ya.ru">Акции</a></li>
+                                              href="">Акции</a></li>
                 <li className="menu__item"><a className="menu__link"
-<<<<<<< HEAD
                                               href="">Магазины</a></li>
             </ul>
-            </div>
-=======
-                                              href="ya.ru">Магазины</a></li>
-            </ul></>
->>>>>>> main
+        </div>
     );
 }
 
