504414b545b842a406ee4639b5c842b3fd78859b
knakna <kna@tut.by>
2021-05-18 23:29:02 +0300

    add table

---
+++ src/pages/card/adress/Adress.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Form from './form/Form';
+import Table from './table/Table';
+
+
+import s from './Adress.module.scss';
+
+
+
+function Adress() {
+  return (
+
+    <div className={s.adress}>
+
+      <Form/>
+
+      <Table/>
+
+
+
+    </div>
+
+  );
+}
+
+export default Adress;
---
+++ src/pages/card/adress/form/Form.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { StyledRadio } from './checkbox/Checkbox'
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+
+import s from './Form.module.scss';
+
+
+
+function Form() {
+  return (
+
+    <form className={s.form}>
+
+
+      <div className={s.shop}>
+
+        <label className={s.label} for="input">Магазин</label>
+        <input className={s.input} id="input" type="text" placeholder="&#921;" />
+
+
+        {/* <label className={s.label} > Магазин
+      <input className={s.input} type="text" autoFocus />
+        </label> */}
+      </div>
+
+        <FormControl component="fieldset"  >
+          <RadioGroup defaultValue="Забрать в течение недели" aria-label="gender" name="customized-radios"
+          style={{flexDirection: "row"}}>
+            <FormControlLabel value="Забрать сегодня" control={<StyledRadio />} label="Забрать сегодня" />
+            <FormControlLabel value="Забрать в течение недели" control={<StyledRadio />} label="Забрать в течение недели" />
+          </RadioGroup>
+        </FormControl>
+
+
+    </form>
+  );
+}
+
+export default Form;
