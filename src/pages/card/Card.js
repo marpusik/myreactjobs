bdd96cbba3851668462f4a88c7460471815e9def
knakna <kna@tut.by>
2021-05-19 18:07:13 +0300

    mod structure

--- src/pages/card/Card.js
+++ src/pages/card/Card.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import Breadcrumb from '../../components/breadcrumb/Breadcrumb';
 import Adress from './adress/Adress';
+import CardFeatures from './cardFeatures/CardFeatures';
+import CardMenu from './cardMenu/CardMenu';
+
 
 
 
 import s from './Card.module.scss';
-import CardFeatures from './cardFeatures/CardFeatures';
-import CardMenu from './cardMenu/CardMenu';
 
 
 
@@ -15,8 +17,8 @@ function Card() {
   return (
 
     <div className={s.card}>
-
-      <CardFeatures/>
+      <Breadcrumb />
+      <CardFeatures />
       <CardMenu />
       <Adress />
 
